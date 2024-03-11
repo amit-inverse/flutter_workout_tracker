dc4333fb1697210190dbbc1bc077929678a76292
Amit Kumar Das <amit.inverse@gmail.com>
2024-03-11 22:28:14 +0530

    007

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 
 import 'src/app.dart';
 
-void main() {
+void main() async {
+  // initialize hive
+  await Hive.initFlutter();
+
+  // open a hive box
+  await Hive.openBox('workout_database');
+
   runApp(const App());
 }
---
+++ lib/src/data/hive_database.dart
@@ -0,0 +1,19 @@
+class HiveDatabase {
+  // reference our hive box
+
+  // check if there is already data stored, if not, record the start date
+
+  // return start date as yyyymmdd
+
+  // write data
+
+  // read data, and return a list of workouts
+
+  // check if any exercise has been done
+
+  // retunr completion status of a given date yyyymmdd
+}
+
+// converts workout objects into a list
+
+// converts the exercises in a workout object into a list of strings
