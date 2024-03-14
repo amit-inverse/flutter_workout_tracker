545de804a32a3012421eb194043b3ac2f272394d
Amit Kumar Das <amit.inverse@gmail.com>
2024-03-14 22:49:25 +0530

    009

--- lib/src/data/hive_database.dart
+++ lib/src/data/hive_database.dart
@@ -1,7 +1,7 @@
 import 'package:hive/hive.dart';
-import 'package:workout_tracker/src/datetime/date_time.dart';
-import 'package:workout_tracker/src/models/exercise_model.dart';
 
+import '../datetime/date_time.dart';
+import '../models/exercise_model.dart';
 import '../models/workout_model.dart';
 
 class HiveDatabase {
@@ -11,24 +11,106 @@ class HiveDatabase {
   // check if there is already data stored, if not, record the start date
   bool previousDataExists() {
     if (_myBox.isEmpty) {
+      // ignore: avoid_print
       print('Previous data does NOT exist');
       _myBox.put('START_DATE', todaysDateYYYYMMDD());
       return false;
     } else {
+      // ignore: avoid_print
       print('Previous data does exist');
       return true;
     }
   }
 
   // return start date as yyyymmdd
+  String getStartDate() {
+    return _myBox.get('START_DATE');
+  }
 
   // write data
+  void saveToDatabase(List<Workout> workouts) {
+    // convert workout objects into lists of strings so that we can save in hive
+    final workoutList = convertObjectToWorkoutList(workouts);
+    final exerciseList = convertObjectToExerciseList(workouts);
+
+    /*
+
+    check if any exercises have been done
+    we will put a 0 or 1 for each yyyymmdd date
+
+    */
+
+    if (exerciseCompleted(workouts)) {
+      _myBox.put("COMPLETION_STATUS_${todaysDateYYYYMMDD()}", 1);
+    } else {
+      _myBox.put("COMPLETION_STATUS_${todaysDateYYYYMMDD()}", 0);
+    }
+
+    // save into hive
+    _myBox.put("WORKOUTS", workoutList);
+    _myBox.put("EXERCISES", exerciseList);
+  }
 
   // read data, and return a list of workouts
+  List<Workout> readFromDatabase() {
+    List<Workout> mySavedWorkouts = [];
+
+    List<String> workoutNames = _myBox.get("WORKOUTS");
+    final exerciseDetails = _myBox.get("EXERCISES");
+
+    // create workout objects
+    for (int i = 0; i < workoutNames.length; i++) {
+      // each workout can have multiple exercises
+      List<Exercise> exercisesInEachWorkout = [];
+
+      for (int j = 0; j < exerciseDetails[i].length; j++) {
+        // add each exercise into a list
+        exercisesInEachWorkout.add(
+          Exercise(
+            name: exerciseDetails[i][j][0],
+            weight: exerciseDetails[i][j][1],
+            reps: exerciseDetails[i][j][2],
+            sets: exerciseDetails[i][j][3],
+            isCompleted: exerciseDetails[i][j][4] == 'true' ? true : false,
+          ),
+        );
+      }
+
+      // create individual workout
+      Workout workout = Workout(
+        name: workoutNames[i],
+        exercises: exercisesInEachWorkout,
+      );
+
+      // add individual workout to overall list
+      mySavedWorkouts.add(workout);
+    }
+
+    return mySavedWorkouts;
+  }
 
   // check if any exercise has been done
+  bool exerciseCompleted(List<Workout> workouts) {
+    // go through each workout
+    for (var workout in workouts) {
+      // go through each exercise in workout
+      for (var exercise in workout.exercises) {
+        if (exercise.isCompleted) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
 
   // retunr completion status of a given date yyyymmdd
+  int getCompletionStatus(String yyyymmdd) {
+    // return 0 or 1, if null then return 0
+    int completionStatus = _myBox.get("COMPLETION_STATUS_$yyyymmdd") ?? 0;
+
+    return completionStatus;
+  }
 }
 
 // converts workout objects into a list --> e.g. [ upperbody, lowerbody ]
